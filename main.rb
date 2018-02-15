fd2ebc2b9140e9a873110574349a0d6a9d9e09af
Eli Boucher <elenaivette@gmail.com>
2018-02-14 19:46:50 -0700

    lets try this again

--- main.rb
+++ main.rb
@@ -7,13 +7,17 @@ class Main
   def self.menu
     puts 'MAIN MENU'.colorize(:cyan)
     puts '1: Enter git command'.colorize(:cyan)
-    puts '2: Exit'.colorize(:cyan)
+    puts '2: Config list'.colorize(:cyan)
+    puts '3: Exit'.colorize(:cyan)
     choice = gets.to_i
     case choice
     when 1
       puts 'Enter git command'.colorize(:green)
       Git.puts_git(gets.strip)
     when 2
+      Git.config
+      exit
+    when 3
       exit
     else
       puts 'Invalid choice'.colorize(:red)
