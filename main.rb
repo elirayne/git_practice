b1573348e8bbbec1ae48f3b2437983bd4526899a
Eli Boucher <elenaivette@gmail.com>
2018-02-14 18:50:10 -0700

    add colorize gem

--- main.rb
+++ main.rb
@@ -1,21 +1,23 @@
+require 'colorize'
+
 def put_git(cmd)
   puts 'git #{cmd} -h'
   menu
 end
 
 def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
+  puts '1: Enter git command'.colorize(:cyan)
+  puts '2: Exit'.colorize(:cyan)
   choice = gets.to_i
     case choice
       when 1
-        puts 'Enter git command'
+        puts 'Enter git command'.colorize(:cyan)
         puts_git(gets.strip)
         menu
       when 2
         exit
       else
-        puts 'Invalid choice'
+        puts 'Invalid choice'.colorize(:red)
         menu
     end 
 end
